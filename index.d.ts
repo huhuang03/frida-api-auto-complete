fc2fb59d94af4bbd0da5aeb722bf68266c9ee6e6
th <huhuang03@gmail.com>
2020-04-20 11:30:07 +0800

    auto commit

---
+++ index.d.ts
@@ -0,0 +1,80 @@
+declare class Backtracer {
+    // static ACCURATE
+}
+
+declare class DebugSymbol {
+    // static fromAddress
+}
+
+declare class Interceptor {
+    static attach(target: string, callbacks: {
+        onEnter: (args: [NativePointer])=>void;
+        onLeave: ()=>void;
+    }): any;
+}
+
+declare class Process {
+    id: number;
+    static enumerateModules(): Module[];
+    static isDebuggerAttached(): boolean;
+    static getCurrentThreadId(): number;
+    static enumerateThreads(): [Thread];
+    static findModuleByName(name: String): Module;
+}
+
+declare class Module {
+    name: string;
+    path: string;
+    base: NativePointer;
+    static findExportByName(moduleName: string, declareName: string): string
+}
+
+declare class Thread {
+    id: number;
+    state: string;
+    static backtrace(context: any, backtracer: any): any;
+}
+
+
+declare class NativePointer {
+    readS8(): number;
+    readU8(): number;
+    readS16(): number;
+    readU16(): number;
+    readS32(): number;
+    readU32(): number;
+    readShort(): number;
+    readUShort(): number;
+    readInt(): number;
+    readUInt(): number;
+    readFloat(): number;
+    readDouble(): number;
+    readPointer(): NativePointer;
+    
+    readCString(size:number): string;
+
+    readUtf8String(size: number): string;
+
+    readUtf16String(size: number): string;
+
+    readAnsiString(size: number): string;
+    
+    isNull(): boolean;
+}
+
+
+// Java
+declare class Java {
+    static perform(func: any): any;
+    
+    static use(pkg: String): any;
+
+    static cast(obj: any, clazz: Class): any;
+}
+
+declare class Class {
+
+}
+
+declare function ptr(address: string): any;
+declare function send(obj: any): any;
--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare const _default: {
-    function(i: number, j: number): void;
-};
-export = _default;
-//# sourceMappingURL=index.d.ts.map
